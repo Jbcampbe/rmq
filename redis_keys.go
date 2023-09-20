098bf624e67b4f944561986f19f894ec4e3a6b96
Josiah Campbell <josiah.campbell@gpsinsight.com>
2023-09-20 10:24:59 -0400

    change ready queue name

--- redis_keys.go
+++ redis_keys.go
@@ -10,7 +10,7 @@ const (
 	connectionQueueUnackedBaseTemplate   = "rmq::connection::{connection}::queue::[{queue}]::unacked"   // List of deliveries consumers of {connection} are currently consuming
 
 	queuesKey                 = "rmq::queues"                     // Set of all open queues
-	queueReadyBaseTemplate    = "rmq::queue::[{queue}]::ready"    // List of deliveries in that {queue} (right is first and oldest, left is last and youngest)
+	queueReadyBaseTemplate    = "{queue}"                         // List of deliveries in that {queue} (right is first and oldest, left is last and youngest)
 	queueRejectedBaseTemplate = "rmq::queue::[{queue}]::rejected" // List of rejected deliveries from that {queue}
 
 	phConnection = "{connection}" // connection name
